9174c3f17c2cef58e188934a6ce42441902bfe52
Marco Zac <zacmarco@gmail.com>
2020-02-07 21:11:28 +0100

    Modified cpp example; upgraded climinalbuilder ver

--- examples/cpp/cpp_cbk.cpp
+++ examples/cpp/cpp_cbk.cpp
@@ -3,11 +3,42 @@
 #include <cstdio>
 #include <string>
 
+namespace {
+
+    std::string endl("\n");
+
+    class stream{
+        private:
+            FILE *file;
+        public:
+            stream(){
+                file=NULL;
+            }
+            stream(FILE *f){
+                file=f;
+            }
+            stream& operator<<(std::string s) {
+                if(file) {
+                    fprintf(file, "%s", s.c_str());
+                }
+                return *this;
+            }
+            stream& operator<<(const char *s) {
+                if(file && s) {
+                    fprintf(file, "%s", s);
+                }
+                return *this;
+            }
+    };
+} 
+
 /* COMMAND: test		( main )		*/
 int test_cbk(FILE* in, FILE* out, const Cmdinfo *info, const char* line, void *cookie)
 {
-    std::string task(*((std::string*)cookie));
-    fprintf(out, "Test callback called on task %s\n", task.c_str());
+    std::string *task=reinterpret_cast<std::string*>(cookie);
+    stream os(out);
+
+    os << "Test callback for thread " << *task << endl;
 
     return 0;
 }
--- tools/climinalbuilder/cli_data.h
+++ tools/climinalbuilder/cli_data.h
@@ -29,8 +29,8 @@
 
 #include "CExportFiles.h"
 
-#define VER_MAJ 0
-#define VER_MIN 1
-#define VER_BUILD 182
+#define VER_MAJ 1
+#define VER_MIN 0
+#define VER_BUILD 0
 
 #endif
