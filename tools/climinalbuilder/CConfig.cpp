46d81cac78fbb021a385f7d9e60ae17a0fdca79b
Marco Zac <zacmarco@gmail.com>
2020-02-26 16:41:54 +0100

    Adding missing files (CI error reported)

---
+++ tools/climinalbuilder/CConfig.cpp
@@ -0,0 +1,42 @@
+/*
+   CliminalBuilder Tool for
+   Climinal
+   Copyright (C) 2020, Marco Zaccheria, Rocco Affinito
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General
+   Public License along with this library; if not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301 USA
+*/
+
+#include "CConfig.h"
+
+//constructor
+CConfig::CConfig()
+{
+    history_size=256;
+}
+
+CConfig::~CConfig() {}
+
+int  CConfig::getHistorySize(void)
+{
+    return history_size;
+}
+
+
+void CConfig::setHistorySize(const unsigned int s)
+{
+    history_size=s;
+}
+
---
+++ tools/climinalbuilder/CConfig.h
@@ -0,0 +1,44 @@
+/*
+   CliminalBuilder Tool for
+   Climinal
+   Copyright (C) 2020, Marco Zaccheria, Rocco Affinito
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General
+   Public License along with this library; if not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301 USA
+*/
+
+#ifndef CCONFIG_H
+#define CCONFIG_H
+
+#ifdef WIN32
+#include "stdafx.h"
+#endif
+
+#include "common.h"
+
+class CConfig
+{
+public:
+  CConfig();
+  ~CConfig();
+
+  int  getHistorySize(void);
+  void setHistorySize(const unsigned int s); 
+
+private:
+  unsigned int history_size;
+};
+
+#endif
